e2ed7e9ff7b03d5e7c03a093b691cd273a5e6efc
Cody Condon <codycondon50@gmail.com>
2017-08-28 11:26:25 -0400

    Initial Commit

---
+++ lagoonzProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lagoonzProject
+//
+//  Created by Cody Condon on 2017-08-28.
+//  Copyright © 2017 Cody Condon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
